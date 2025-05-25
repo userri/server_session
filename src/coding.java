778dcf555153b074267054de72ac5be86b67d057
Minu Kim <kmw106933@naver.com>
2025-05-26 01:48:32 +0900

    initial commit

---
+++ src/coding.java
@@ -0,0 +1,12 @@
+class Team {
+    int id;
+    int[] scoreList;
+    int lastSubmit;
+    int totalScore;
+}
+
+public class coding {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
